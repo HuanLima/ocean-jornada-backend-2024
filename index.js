f380fc62c83ccb121912e55ef096d3ba7e1966f2
Huanderson Lima <81698521+huanlima@users.noreply.github.com>
2024-04-12 20:12:24 +0000

    Endpoint `[GET] /item` criado

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require("express")
 const app = express()
 
 app.get("/", function (req, res) {
-    res.send("Hello world!")
+    res.send("Hello, world!")
 })
 
 app.get("/oi", function (req, res) {
-    res.send("Olá mundo!")
+    res.send("Olá, mundo!")
+})
+
+// Lista de personagens
+const lista = ["Rick Sanchez", "Morty Smith", "Summer Smith"]
+
+// Read All -> [GET /item]
+app.get("/item", function (req, res) {
+    res.send(lista)
 })
 
 app.listen(3000)
