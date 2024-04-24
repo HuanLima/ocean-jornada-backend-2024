f0e71c8be1e2131f03d8306942b450a1f493cebb
Huanderson Lima <81698521+huanlima@users.noreply.github.com>
2024-04-24 17:06:02 +0000

    Endpoint `[PUT] /item/:id` criado

--- index.js
+++ index.js
@@ -13,40 +13,53 @@ app.get("/oi", function (req, res) {
 const lista = ["Rick Sanchez", "Morty Smith", "Summer Smith"]
 //                  0                1               2
 
-// Read All -> [GET] /item
+// Endpoint Read All -> [GET] /item
 app.get("/item", function (req, res) {
     // Enivo a lista inteira como resposta HTTP
     res.send(lista)
 })
 
-// Read by ID -> [GET] /item/:id
+// Endpoint Read by ID -> [GET] /item/:id
 app.get("/item/:id", function (req, res) {
     // Acesso o ID no parâmetro de rota
     const id = req.params.id
 
-    // Acesso um item na lista baseado no ID recebido
-    const item = lista[id]
+    // Acesso um item na lista com base no ID recebido (usando o "id-1"), e coloco este item na variável item
+    const item = lista[id - 1]
 
-    // Enivo o item obtido como resposta HTTP
+    // Envio o item obtido como resposta HTTP
     res.send(item)
 } )
 
 // Sinalizamos que o corpo da requisição está em JSON
 app.use(express.json())
 
-// Create -> [POST] /item
+// Endpoint Create -> [POST] /item
 app.post("/item", function (req, res) {
-    // Extraímos o corpo da requisição
-    const body = req.body
+    // Extraímos o item através do corpo da requisição.
+    // No objeto JSON, pegamos o nome(string) que foi enviado dentro do corpo da requisição.
+    const item = req.body.nome
 
-    // Pegamos o nome(string) que foi enviado dentro do corpo
-    const item = body.nome
-
-    // Colocamos o nome dentro da lista de itens
+    // Adicionamos o nome obtido na lista de itens
     lista.push(item)
 
     // Enviamos uma resposta de sucesso
-    res.send("Item adicionado com sucesso!"+ " : " + item)
+    res.send("Item adicionado com sucesso: " + item)
 } )
+
+// Endpoint Update -> [PUT] /item/:id
+app.put("/item/:id", function (req, res) {
+    // Obtemos o id do parâmetro de rota
+    const id = req.params.id
+
+    // Obtemos o corpo da requisição HTTP para sabermos qual o novo valor
+    const novoItem = req.body.nome
+
+    // Atualizamos o novo item (com o seu novo valor) na lista
+    lista[id - 1] = novoItem
+
+    // Enviamos uma mensagem de sucesso
+    res.send("Item atualizado com sucesso: " + id + " . Novo valor do item: " + novoItem)
+})
 
 app.listen(3000)
